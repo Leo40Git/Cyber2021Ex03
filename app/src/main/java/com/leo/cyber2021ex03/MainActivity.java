410fdd0b773ba7a738bc8375b55ab8c1390d3c7a
Kfir Awad <kfir40mailminer@gmail.com>
2018-12-10 09:30:00 +0200

    programming

--- app/src/main/java/com/leo/cyber2021ex03/MainActivity.java
+++ app/src/main/java/com/leo/cyber2021ex03/MainActivity.java
@@ -2,12 +2,28 @@ package com.leo.cyber2021ex03;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private int clicks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        clicks = 0;
+    }
+
+    public void onButtonClick(View view) {
+        clicks++;
+        if (clicks > 6) {
+            clicks = 0;
+            button.setText("Enough clicks. From the top!");
+        } else
+            button.setText("Click number: " + clicks);
     }
 }
